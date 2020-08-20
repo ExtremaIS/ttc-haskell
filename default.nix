adb9635af808e12b9af44f64175ff1c35e1439d1
Travis Cardwell <travis.cardwell@extrema.is>
2020-08-21 08:38:08 +0900

    Update Nix configuration

--- default.nix
+++ default.nix
@@ -1,2 +1,35 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc8101" }:
-nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./ttc.nix { }
+let
+
+  nixpkgsRev = "5272327b81ed";
+  compilerVersion = "ghc8101";
+
+  githubTarball = owner: repo: rev:
+    builtins.fetchTarball { url = "https://github.com/${owner}/${repo}/archive/${rev}.tar.gz"; };
+
+  gitIgnore = pkgs.nix-gitignore.gitignoreSourcePure;
+
+  config = {
+    packageOverrides = super: let self = super.pkgs; in rec {
+      haskell = super.haskell // {
+        packageOverrides = self: super: {
+          haskell-nix = super.callCabal2nix "haskell-nix" (gitIgnore [./.gitignore] ./.) {};
+        };
+      };
+    };
+  };
+
+  pkgs = import (githubTarball "NixOS" "nixpkgs" nixpkgsRev) { inherit config; };
+  compilerSet = pkgs.haskell.packages."${compilerVersion}";
+
+in {
+
+  inherit pkgs;
+
+  shell = compilerSet.shellFor {
+    packages = p: [p.haskell-nix];
+    buildInputs = with pkgs; [
+      compilerSet.cabal-install
+    ];
+  };
+
+}
