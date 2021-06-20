91bf21765fec6bb922a4d183979fb48b3e8c3293
Divam <dfordivam@gmail.com>
2021-06-20 14:18:00 +0900

    * Add release.nix and thunks to allow building on various versions of ghc * Use nix thunks in shell.nix * Remove default.nix

--- shell.nix
+++ shell.nix
@@ -1,1 +1,10 @@
-(import ./default.nix).shell
+{ pkgs ? import ./nix/default {}
+, compiler ? "ghc8103"
+}:
+let
+  gitIgnore = (import ./nix/default {}).nix-gitignore.gitignoreSourcePure;
+in
+  pkgs.haskell.packages.${compiler}.developPackage {
+    root = gitIgnore [./.gitignore] ./.;
+    name = "ttc";
+  }
